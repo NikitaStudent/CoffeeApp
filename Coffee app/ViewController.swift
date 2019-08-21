732e138aa85a43b8267c4f3fb3776dc5dbf104e8
NikitaStudent <NKA_2000_00@mail.ru>
2019-08-21 13:35:46 +0300

    Initial Commit

---
+++ Coffee app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coffee app
+//
+//  Created by Никита Кожевников on 21/08/2019.
+//  Copyright © 2019 Beet inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
